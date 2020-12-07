9a7cae8adc29e7b3c6cafb9b97ca0108b86f17e0
Jared R Clemence <jaredclemence@gmail.com>
2020-12-07 13:07:34 -0800

    Updated lead letter language.

--- resources/views/emails/lead-notice.blade.php
+++ resources/views/emails/lead-notice.blade.php
@@ -2,7 +2,9 @@ Nick:
 
 We have a new lead looking into {{$address}} in {{$city}}, {{$state}} {{$zip}}. The lead received a copy of the brochure 15 minutes ago and is likely ready to receive a follow-up phone call.
 
-Please note that by working this lead that you agree to our oral referral agreement of 25% to Jared Clemence. The information below has been provided by the user, so I cannot be certain that it is 100% correct.
+Please note that this lead is sent to you as part of our oral referral agreement, which is that you agree to call the lead and pay 25% referral fee to Jared Clemence, in exchange, I transfer the lead to you and agree not to follow up.
+
+The information below has been provided by the user, so I cannot be certain that it is 100% correct. If you do not wish to work this lead, please let me know as soon as possible so that I may follow up accordingly.
 
 Lead Name: {{$name}}
 Lead Phone: {{$phone}}
