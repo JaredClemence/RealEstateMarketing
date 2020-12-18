6d2034596b4c0f5c66ac748a581b7c4e35a0f666
Jared R Clemence <jaredclemence@gmail.com>
2020-12-17 22:14:17 -0500

    Property Factory implemented.

---
+++ database/factories/PropertyFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Property;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PropertyFactory extends Factory {
+
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Property::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition() {
+        return [
+            'status' => 'ACTIVE',
+            'status_date' => now(),
+            'address' => $this->faker->streetAddress,
+            'city' => 'Bakersfield',
+            'state' => 'CA',
+            'zip' => $this->faker->postcode,
+            'beds' => rand(2, 5),
+            'baths' => rand(1, 3),
+            'sqft' => rand(1000, 2500),
+            "teaser_prompt" => "Download this eBrochure",
+            "teaser_text" => "Thank you for downloading this eBrochure",
+            'virtual_embed'=>''
+        ];
+    }
+
+}
