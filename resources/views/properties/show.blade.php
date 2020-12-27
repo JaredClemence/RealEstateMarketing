726e7dae7fdd347ea08b05a11adc8cd6a9bc5463
Jared R Clemence <jaredclemence@gmail.com>
2020-12-26 20:20:16 -0800

    Added means of creating new webinar events.

--- resources/views/properties/show.blade.php
+++ resources/views/properties/show.blade.php
@@ -64,6 +64,16 @@
                     @foreach($property->webinars as $webinar)
                     <x-properties.webinar.list-item :webinar="$webinar"></x-properties.webinar.list-item>
                     @endforeach
+                    <li class='list-group-item'>
+                        <form action='{{route('webinar.store',compact('property'))}}' method='POST'>
+                            @csrf
+                            @method('POST')
+                            <x-form-text-input type='text' name='link'>YouTube Link</x-form-text-input>
+                            <x-form-text-input type='text' name='day_of_week'>Day of Week</x-form-text-input>
+                            <x-form-text-input type='text' name='time'>Time</x-form-text-input>
+                            <button type='submit' class='btn btn-primary'>Create New Webinar</button>
+                        </form>
+                    </li>
                 </div>
             </div>
             <div class='col-12 col-md-6'>
