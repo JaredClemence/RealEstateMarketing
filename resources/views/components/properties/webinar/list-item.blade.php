4aa1d2708b9bc2f0814e99177ec7505aaa73603c
Jared R Clemence <jaredclemence@gmail.com>
2020-12-26 20:26:28 -0800

    Added link to webinar url text.

--- resources/views/components/properties/webinar/list-item.blade.php
+++ resources/views/components/properties/webinar/list-item.blade.php
@@ -6,7 +6,10 @@ $property = $webinar->property
         
         @csrf
         @method('DELETE')
-        Public Url: <span class='d-inline'>{{route('public.webinar.show',compact('webinar','property'))}}</span>  
+        @php
+        $routeUrl = route('public.webinar.show',compact('webinar','property'));
+        @endphp
+        Public Url: <a href="{{$routeUrl}}" target="_blank">{{$routeUrl}}</a>  
         <br/>Embeds: <span class='d-inline'>{{$webinar->link}}</span>
         <br/>Day: <span class='d-inline'>{{$webinar->day_of_week}} {{$webinar->time->format('H:i')}}</span> <button type='submit' class='btn btn-link'>Delete</button>
     </form>
